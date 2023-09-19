fa409f1325f6f11821320004799d082c0db53226
Alex Tibbs <alex.tibbs@cabinetoffice.gov.uk>
2023-09-19 12:23:44 +0100

    Tidy

--- data_utils.py
+++ data_utils.py
@@ -3,10 +3,13 @@ import requests as requests
 
 BIKE_POINT_URL = "https://api.tfl.gov.uk/BikePoint/"
 
+
 def get_bike_point_data():
     response = requests.get(BIKE_POINT_URL)
     data = response.json()
-    props = {point["id"]: {"common_name": point["commonName"], "lat": point["lat"], "lon": point["lon"]} for point in data}
+    props = {
+        point["id"]: {"common_name": point["commonName"], "lat": point["lat"], "lon": point["lon"]} for point in data
+    }
     additional_props = {point["id"]: point["additionalProperties"] for point in data}
     additional_props = {k: process_additional_properties(v) for k, v in additional_props.items()}
     return pandas.DataFrame({k: {**props[k], **additional_props[k]} for k in props}).T
