e3629b4511e21b0336125a277f61880c2e9b5718
Alex Tibbs <alex.tibbs@cabinetoffice.gov.uk>
2023-09-19 14:12:41 +0100

    Get obesity prevalance data

--- data_utils.py
+++ data_utils.py
@@ -1,7 +1,13 @@
+import io
+
 import pandas
 import requests as requests
+import streamlit
 
 BIKE_POINT_URL = "https://api.tfl.gov.uk/BikePoint/"
+FINGERTIPS_URL = "https://fingertips.phe.org.uk/api/all_data/csv/for_one_indicator"
+
+LATEST_PERIOD = "2019/20 - 21/22"
 
 
 def get_bike_point_data():
@@ -18,3 +24,21 @@ def get_bike_point_data():
 def process_additional_properties(ap):
     df = pandas.DataFrame(ap)
     return df.set_index("key")["value"].to_dict()
+
+
+def get_fingertips_data_by_indicator_id(indicator_id):
+    # Docs for this endpoint: https://fingertips.phe.org.uk/api#!/Data/Data_GetDataFileForOneIndicator
+    url = FINGERTIPS_URL
+    params = {"indicator_id": indicator_id}
+    response = requests.get(url, params)
+    return pandas.read_csv(io.StringIO(response.text), low_memory=False)
+
+
+@streamlit.cache_data
+def get_prevalance_of_overwieght_year_6():
+    # ID is from https://fingertips.phe.org.uk/profile/national-child-measurement-programme/data
+    indicator_id = 93108
+    df = get_fingertips_data_by_indicator_id(indicator_id)
+    df = df[df["Area Type"] == "MSOA"]
+    df = df[df["Time period"] == LATEST_PERIOD]
+    return df[["Area Code", "Area Name",  "Value"]]
