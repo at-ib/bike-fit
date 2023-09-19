592bd4a0a0590f67d89a7182a79d6b955e499065
Alex Tibbs <alex.tibbs@cabinetoffice.gov.uk>
2023-09-19 12:23:25 +0100

    Get bike point data

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # bike-fit
+
 A project to explore how we can encourage people to exercise using London's bike sharing scheme
 
 ## Formatting
---
+++ data_utils.py
@@ -0,0 +1,17 @@
+import pandas
+import requests as requests
+
+BIKE_POINT_URL = "https://api.tfl.gov.uk/BikePoint/"
+
+def get_bike_point_data():
+    response = requests.get(BIKE_POINT_URL)
+    data = response.json()
+    props = {point["id"]: {"common_name": point["commonName"], "lat": point["lat"], "lon": point["lon"]} for point in data}
+    additional_props = {point["id"]: point["additionalProperties"] for point in data}
+    additional_props = {k: process_additional_properties(v) for k, v in additional_props.items()}
+    return pandas.DataFrame({k: {**props[k], **additional_props[k]} for k in props}).T
+
+
+def process_additional_properties(ap):
+    df = pandas.DataFrame(ap)
+    return df.set_index("key")["value"].to_dict()
