5d849842b789880a893d99d7fb3949729a1cc14c
Alex Tibbs <alex.tibbs@cabinetoffice.gov.uk>
2023-09-20 11:24:48 +0100

    Add usage

--- analysis.py
+++ analysis.py
@@ -1,3 +1,4 @@
+import pandas
 import streamlit
 from geopandas import GeoDataFrame
 
@@ -7,14 +8,50 @@ from map_utils import get_london_msoa_boundaries
 
 from shapely.geometry import Point
 
+from settings import DATA_DIR
+
+USAGE_FILE = "374JourneyDataExtract12Jun2023-18Jun2023.csv"
+
 
 # @streamlit.cache_data
-def get_number_of_docks_per_msoa():
-    bike_points = get_bike_point_data()
+def get_number_of_docks_per_msoa(bike_points):
+    bike_points = add_msoa_data(bike_points)
+    docks = bike_points[["NbDocks", AREA_CODE_FIELD]].astype({"NbDocks": int, AREA_CODE_FIELD: str}).groupby(AREA_CODE_FIELD, as_index=False).sum()
+    return docks.rename(columns={"MSOA11CD": "Area Code", "NbDocks": "Value"})
+
+
+def add_msoa_data(bike_points):
     geometry = bike_points.apply(lambda x: Point(x["lon"], x["lat"]), axis=1)
     # To assign BikePoints to MSOAs we need to uses geopandas.sjoin, which means converting to GeoDataFrame
     bike_points = GeoDataFrame(bike_points.assign(geometry=geometry))
     boundaries = GeoDataFrame.from_features(get_london_msoa_boundaries())
     bike_points = bike_points.sjoin(boundaries)
-    docks = bike_points[["NbDocks", AREA_CODE_FIELD]].astype({"NbDocks": int, AREA_CODE_FIELD: str}).groupby(AREA_CODE_FIELD, as_index=False).sum()
-    return docks.rename(columns={"MSOA11CD": "Area Code", "NbDocks": "Value"})
+    return bike_points
+
+
+def get_usage_by_msoa(bike_points):
+    usage = get_usage_by_bike_point()
+    # Zero pad station numbers to match bike_points
+    usage = usage.assign(station_number=usage["station_number"].map("{:06d}".format))
+    bike_points = bike_points.merge(usage, left_on="TerminalName", right_on="station_number")
+    bike_points = add_msoa_data(bike_points)
+    #  TODO generalise this with get_number_of_docks_per_msoa
+    return bike_points[["starts", "ends", AREA_CODE_FIELD]].groupby(AREA_CODE_FIELD, as_index=False).sum()
+
+
+@streamlit.cache_data
+def get_number_of_journey_starts_or_ends_per_msoa(bike_points, starts_or_ends):
+    df = get_usage_by_msoa(bike_points)
+    return df[[AREA_CODE_FIELD, starts_or_ends]].rename(columns={AREA_CODE_FIELD: "Area Code", starts_or_ends: "Value"})
+
+
+def get_usage_by_bike_point():
+    df = pandas.read_csv(DATA_DIR / USAGE_FILE)
+    # With more time I would look at journey duration and data for a longer time period
+    df = pandas.concat([count_vals(df["Start station number"], "starts"), count_vals(df["End station number"], "ends")], axis=1)
+    return df.reset_index(names="station_number")
+
+
+def count_vals(ser, name):
+    # There's probably a more elegant way to do this
+    return pandas.DataFrame(ser.value_counts()).rename(columns={"count": name})
--- pages/Map.py
+++ pages/Map.py
@@ -1,7 +1,7 @@
 import streamlit
 from streamlit_folium import st_folium
 
-from analysis import get_number_of_docks_per_msoa
+from analysis import get_number_of_docks_per_msoa, get_number_of_journey_starts_or_ends_per_msoa
 from data_utils import get_bike_point_data, get_prevalance_of_overwieght_year_6, get_population_by_msoa, \
     get_number_of_obese_children
 from map_utils import add_boundary_layer, get_london_msoa_boundaries, add_choropleth_layer, \
@@ -13,7 +13,9 @@ LAYER_NAMES = {
     "obese_prev": "Obesity prevalance choropleth",
     "pop": "Population choropleth",
     "obese_num": "Number of obese children",
-    "docks": "Number of BikePoint docks per MSOA"
+    "docks": "Number of BikePoint docks per MSOA",
+    "starts": "Number of journeys started in each MSOA",
+    "ends": "Number of journeys ended in each MSOA",
 }
 
 def page_content():
@@ -30,9 +32,11 @@ def page_content():
     # With more time I would restructure this "if, elif" ladder probably using a dictionary
     if layer_to_show == LAYER_NAMES["boundaries"]:
         m = add_boundary_layer(boundaries, m)
+        m = add_bike_points(bike_points, m)
     elif layer_to_show == LAYER_NAMES["obese_prev"]:
         df = get_prevalance_of_overwieght_year_6()
         m = add_choropleth_layer(boundaries, m, df)
+        m = add_bike_points(bike_points, m)
         notes = """
         This obesity measure is only for year 6 children. With more time I would have looked for data for adults.
         
@@ -41,18 +45,27 @@ def page_content():
     elif layer_to_show == LAYER_NAMES["pop"]:
         df = get_population_by_msoa()
         m = add_choropleth_layer(boundaries, m, df)
+        m = add_bike_points(bike_points, m)
         notes = """
         With more time I would have looked at where businesses and attractions are.
         """
     elif layer_to_show == LAYER_NAMES["obese_num"]:
         df = get_number_of_obese_children()
         m = add_choropleth_layer(boundaries, m, df)
+        m = add_bike_points(bike_points, m)
         notes = "Potential policy option: A collection of BikePoints in East London around Barking"
     elif layer_to_show == LAYER_NAMES["docks"]:
-        df = get_number_of_docks_per_msoa()
+        df = get_number_of_docks_per_msoa(bike_points)
         m = add_choropleth_layer(boundaries, m, df)
         notes = "With more time I would have done the number of docks per area"
-    m = add_bike_points(bike_points, m)
+    elif layer_to_show == LAYER_NAMES["starts"]:
+        df = get_number_of_journey_starts_or_ends_per_msoa(bike_points, "starts")
+        m = add_choropleth_layer(boundaries, m, df)
+        notes = "With more time I would have normalised by area"
+    elif layer_to_show == LAYER_NAMES["ends"]:
+        df = get_number_of_journey_starts_or_ends_per_msoa(bike_points, "ends")
+        m = add_choropleth_layer(boundaries, m, df)
+        notes = "With more time I would have normalised by area"
     streamlit.markdown(notes)
     st_folium(m, height=700, width=900, use_container_width=False, returned_objects=[])
 
